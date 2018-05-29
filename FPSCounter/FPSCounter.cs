08181143c4236851b7e7cfc03528c55f23f95821
MarC0 <dummyaccx@gmail.com>
2018-05-29 13:08:21 +0200

    Moved settings to ConfigWrappers; Use Configuration Manager for settings, Changed keybind to U+Shift; Version bump to 1.2

--- FPSCounter/FPSCounter.cs
+++ FPSCounter/FPSCounter.cs
@@ -1,70 +1,46 @@
 ﻿using BepInEx;
-using System;
-using System.IO;
-using System.Reflection;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace FPSCounter
 {
-    [BepInPlugin("FPSCounter", "FPS Counter", "1.1")]
+    [BepInPlugin("MarC0.FPSCounter", "FPS Counter", "1.2")]
     public class FPSCounter : BaseUnityPlugin
     {
-        private static readonly string ConfigFileLocation;
         private readonly float avgFraction = 0.994f;
         private readonly int screenOffset = 20;
 
-        private State currentState = State.Normal;
+        public SavedKeyboardShortcut ShowCounter { get; }
+
+        public ConfigWrapper<TextAnchor> Position { get; }
+        private ConfigWrapper<State> CurrentState { get; }
+
+        private State lastState;
+
         private float deltaTime;
-        private bool initial;
         private float lastAvg;
         private float minFps, maxFps;
 
         private Rect screenRect;
         private GUIStyle style = new GUIStyle();
 
-        static FPSCounter()
+        public FPSCounter()
         {
-            ConfigFileLocation = Assembly.GetExecutingAssembly().Location + ".config";
+            ShowCounter = new SavedKeyboardShortcut("Toggle FPS counter", this, new KeyboardShortcut(KeyCode.U, false, false, true));
+            Position = new ConfigWrapper<TextAnchor>("Counter screen position", this, TextAnchor.LowerLeft);
+            CurrentState = new ConfigWrapper<State>("State of the counter", this, State.VisibleWhite);
         }
 
         private enum State
         {
             Hidden,
-            Normal,
-            NormalWhite,
-        }
-
-        private System.Collections.IEnumerator HideKeybindInfo()
-        {
-            yield return new WaitForSecondsRealtime(15);
-
-            initial = false;
-        }
-
-        private void LoadConfig()
-        {
-            if (File.Exists(ConfigFileLocation))
-            {
-                initial = false;
-                try
-                {
-                    currentState = (State)int.Parse(File.ReadAllText(ConfigFileLocation));
-                }
-                catch (Exception ex)
-                {
-                    BepInLogger.Log("Failed to load FPSCounter config: " + ex.Message);
-                }
-            }
-            else
-            {
-                initial = true;
-            }
+            VisibleWhite,
+            VisibleBlack,
         }
 
         private void OnGUI()
         {
-            if (currentState == State.Hidden) return;
+            if (CurrentState.Value == State.Hidden) return;
 
             float msec = deltaTime * 1000.0f;
             float fps = 1.0f / deltaTime;
@@ -75,9 +51,6 @@ namespace FPSCounter
             if (fps > maxFps) maxFps = fps;
 
             string text = string.Format("{1:0.} FPS, {0:0.0}ms\nAvg: {2:0.}, Min: {3:0.}, Max: {4:0.}", msec, fps, lastAvg, minFps, maxFps);
-
-            if (initial)
-                text += "\nPress U to toggle/reset the counters.\nMade by MarC0";
 
             GUI.Label(screenRect, text, style);
         }
@@ -98,52 +71,38 @@ namespace FPSCounter
 
             style.fontSize = h / 50;
 
-            if (currentState == State.Normal)
+            if (CurrentState.Value == State.VisibleBlack)
                 style.normal.textColor = Color.black;
-            else if (currentState == State.NormalWhite)
+            else if (CurrentState.Value == State.VisibleWhite)
                 style.normal.textColor = Color.white;
-        }
-
-        private void SaveConfig()
-        {
-            try
-            {
-                File.WriteAllText(ConfigFileLocation, ((int)currentState).ToString());
-            }
-            catch (Exception ex)
-            {
-                BepInLogger.Log("Failed to save FPSCounter config: " + ex.Message);
-            }
         }
 
         private void Start()
         {
-            style.alignment = TextAnchor.UpperLeft;
-
-            LoadConfig();
-
             ResetValues();
-
-            if (initial)
-                StartCoroutine(HideKeybindInfo());
 
             SceneManager.sceneLoaded += OnLevelFinishedLoading;
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.U))
+            if (ShowCounter.IsDown())
             {
-                currentState = currentState.Next();
-                initial = false;
-
-                ResetValues();
-
-                SaveConfig();
+                lastState = CurrentState.Value.Next();
+                CurrentState.Value = lastState;
             }
 
-            if (currentState == State.Hidden) return;
+            var state = CurrentState.Value;
 
+            if (state != lastState)
+            {
+                ResetValues();
+                lastState = state;
+            }
+
+            if (state == State.Hidden) return;
+
+            style.alignment = Position.Value;
             deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
         }
     }
