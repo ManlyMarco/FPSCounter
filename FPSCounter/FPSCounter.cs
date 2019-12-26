79b968962e0cecb826c54fa1d8407b27b7441f9a
MarC0 <39247311+ManlyMarco@users.noreply.github.com>
2019-12-26 13:22:43 +0100

    Change only show fps setting to show detailed stats

--- FPSCounter/FPSCounter.cs
+++ FPSCounter/FPSCounter.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Diagnostics;
 using BepInEx;
 using BepInEx.Configuration;
@@ -17,8 +17,8 @@ namespace FPSCounter
         private static ConfigEntry<CounterColors> _counterColor;
         private static ConfigEntry<TextAnchor> _position;
         private static ConfigEntry<bool> _shown;
-        private static ConfigEntry<bool> _pluginStats;
-        private static ConfigEntry<bool> _onlyFPS;
+        private static ConfigEntry<bool> _showPluginStats;
+        private static ConfigEntry<bool> _showUnityMethodStats;
 
         internal static new ManualLogSource Logger;
 
@@ -28,8 +28,8 @@ namespace FPSCounter
 
             _showCounter = Config.Bind("General", "Toggle counter and reset stats", new KeyboardShortcut(KeyCode.U, KeyCode.LeftShift), "Key to enable and disable the plugin.");
             _shown = Config.Bind("General", "Enable", false, "Monitor performance statistics and show them on the screen. When disabled the plugin has no effect on performance.");
-            _pluginStats = Config.Bind("General", "Enable monitoring plugins", true, "Count time each plugin takes every frame to execute. Only detects MonoBehaviour event methods, so results might be lower than expected. Has a small performance penalty.");
-            _onlyFPS = Config.Bind("General", "Only FPS", false, "Only show and calculate the FPS.");
+            _showPluginStats = Config.Bind("General", "Enable monitoring plugins", true, "Count time each plugin takes every frame to execute. Only detects MonoBehaviour event methods, so results might be lower than expected. Has a small performance penalty.");
+            _showUnityMethodStats = Config.Bind("General", "Show detailed frame stats", true, "Show how much time was spent by Unity in each part of the frame, for example how long it took to run all Update methods.");
 
             _position = Config.Bind("Interface", "Screen position", TextAnchor.LowerRight, "Which corner of the screen to display the statistics in.");
             _counterColor = Config.Bind("Interface", "Color of the text", CounterColors.White, "Color of the displayed stats. Outline has a performance hit but it always easy to see.");
@@ -44,7 +44,7 @@ namespace FPSCounter
 
             OnEnable();
         }
-        
+
         private void Update()
         {
             if (_showCounter.Value.IsDown())
@@ -68,7 +68,7 @@ namespace FPSCounter
                 if (_helpers[0] == null) _helpers[0] = gameObject.AddComponent<FrameCounterHelper>();
                 if (_helpers[1] == null) _helpers[1] = gameObject.AddComponent<FrameCounterHelper.FrameCounterHelper2>();
 
-                if (_pluginStats.Value && !_onlyFPS.Value)
+                if (_showPluginStats.Value)
                     PluginCounter.Start(_helpers[0], this);
             }
         }
@@ -224,11 +224,8 @@ namespace FPSCounter
                     var avgFrame = _frameTime.GetAverage();
                     var frameTimeScaled = avgFrame / _nanosecPerTick;
                     var fps = 1000000f / frameTimeScaled;
-                    var msScale = 1f / (_nanosecPerTick * 1000f);
 
-                    _outputText = $"{fps:0.0} FPS";
-
-                    if (!_onlyFPS.Value)
+                    if (_showUnityMethodStats.Value)
                     {
                         var avgFixed = _fixedUpdateTime.GetAverage();
                         var avgUpdate = _updateTime.GetAverage();
@@ -238,12 +235,16 @@ namespace FPSCounter
                         var avgGui = _onGuiTime.GetAverage();
 
                         var totalCapturedTicks = avgFixed + avgUpdate + avgYield + avgLate + avgRender + avgGui;
-
                         var otherTicks = avgFrame - totalCapturedTicks;
 
-                        _outputText = _outputText + $"{avgFrame * msScale,5:0.0}ms\nFixed: {avgFixed * msScale,5:0.0}ms\nUpdate: {avgUpdate * msScale,5:0.0}ms\nYield/anim: {avgYield * msScale,5:0.0}ms\nLate: {avgLate * msScale,5:0.0}ms\nRender/VSync: {avgRender * msScale,5:0.0}ms\nOnGUI: {avgGui * msScale,5:0.0}ms\nOther: {otherTicks * msScale,5:0.0}ms";
-                    }
+                        var msScale = 1f / (_nanosecPerTick * 1000f);
 
+                        _outputText = $"{fps:0.0} FPS, {avgFrame * msScale,5:0.0}ms\nFixed: {avgFixed * msScale,5:0.0}ms\nUpdate: {avgUpdate * msScale,5:0.0}ms\nYield/anim: {avgYield * msScale,5:0.0}ms\nLate: {avgLate * msScale,5:0.0}ms\nRender/VSync: {avgRender * msScale,5:0.0}ms\nOnGUI: {avgGui * msScale,5:0.0}ms\nOther: {otherTicks * msScale,5:0.0}ms";
+                    }
+                    else
+                    {
+                        _outputText = $"{fps:0.0} FPS";
+                    }
                 }
             }
 
