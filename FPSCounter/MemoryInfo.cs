5b75d170d4bb0ee1a879c3238c9085175738c140
ManlyMarco <dummyaccx@gmail.com>
2020-03-29 23:11:52 +0200

    Fix field order

--- FPSCounter/MemoryInfo.cs
+++ FPSCounter/MemoryInfo.cs
@@ -36,8 +36,7 @@ namespace FPSCounter
         private static extern IntPtr GetCurrentProcess();
 
         [DllImport("psapi.dll", SetLastError = true)]
-        private static extern bool GetProcessMemoryInfo(IntPtr hProcess, [In] [Out] PROCESS_MEMORY_COUNTERS counters,
-            uint size);
+        private static extern bool GetProcessMemoryInfo(IntPtr hProcess, [In] [Out] PROCESS_MEMORY_COUNTERS counters, uint size);
 
         /// <summary>
         /// contains information about the current state of both physical and virtual memory, including extended memory
@@ -57,26 +56,14 @@ namespace FPSCounter
             public uint dwMemoryLoad;
 
             /// <summary>
-            /// Size of unreserved and uncommitted memory in the extended portion of the virtual address space of the calling
-            /// process, in bytes.
+            /// Total size of physical memory, in bytes.
             /// </summary>
-            public ulong ullAvailExtendedVirtual;
-
-            /// <summary>
-            /// Size of available memory to commit, in bytes. The limit is ullTotalPageFile.
-            /// </summary>
-            public ulong ullAvailPageFile;
+            public ulong ullTotalPhys;
 
             /// <summary>
             /// Size of physical memory available, in bytes.
             /// </summary>
             public ulong ullAvailPhys;
-
-            /// <summary>
-            /// Size of unreserved and uncommitted memory in the user mode portion of the virtual address space of the calling
-            /// process, in bytes.
-            /// </summary>
-            public ulong ullAvailVirtual;
 
             /// <summary>
             /// Size of the committed memory limit, in bytes. This is physical memory plus the size of the page file, minus a small
@@ -85,14 +72,26 @@ namespace FPSCounter
             public ulong ullTotalPageFile;
 
             /// <summary>
-            /// Total size of physical memory, in bytes.
+            /// Size of available memory to commit, in bytes. The limit is ullTotalPageFile.
             /// </summary>
-            public ulong ullTotalPhys;
+            public ulong ullAvailPageFile;
 
             /// <summary>
             /// Total size of the user mode portion of the virtual address space of the calling process, in bytes.
             /// </summary>
             public ulong ullTotalVirtual;
+
+            /// <summary>
+            /// Size of unreserved and uncommitted memory in the user mode portion of the virtual address space of the calling
+            /// process, in bytes.
+            /// </summary>
+            public ulong ullAvailVirtual;
+
+            /// <summary>
+            /// Size of unreserved and uncommitted memory in the extended portion of the virtual address space of the calling
+            /// process, in bytes.
+            /// </summary>
+            public ulong ullAvailExtendedVirtual;
 
             /// <summary>
             /// Initializes a new instance of the <see cref="T:MEMORYSTATUSEX" /> class.
@@ -117,25 +116,19 @@ namespace FPSCounter
             public uint PageFaultCount;
 
             /// <summary>
-            /// The Commit Charge value in bytes for this process (SIZE_T). Commit Charge is the total amount of memory that the
-            /// memory manager has committed for a running process.
-            /// </summary>
-            public ulong PagefileUsage;
-
-            /// <summary>
-            /// The peak value in bytes of the Commit Charge during the lifetime of this process (SIZE_T).
-            /// </summary>
-            public ulong PeakPagefileUsage;
-
-            /// <summary>
             /// The peak working set size, in bytes (SIZE_T).
             /// </summary>
             public ulong PeakWorkingSetSize;
 
             /// <summary>
-            /// The current nonpaged pool usage, in bytes (SIZE_T).
+            /// The current working set size, in bytes (SIZE_T).
             /// </summary>
-            public ulong QuotaNonPagedPoolUsage;
+            public ulong WorkingSetSize;
+
+            /// <summary>
+            /// The peak paged pool usage, in bytes (SIZE_T).
+            /// </summary>
+            public ulong QuotaPeakPagedPoolUsage;
 
             /// <summary>
             /// The current paged pool usage, in bytes (SIZE_T).
@@ -148,14 +141,20 @@ namespace FPSCounter
             public ulong QuotaPeakNonPagedPoolUsage;
 
             /// <summary>
-            /// The peak paged pool usage, in bytes (SIZE_T).
+            /// The current nonpaged pool usage, in bytes (SIZE_T).
             /// </summary>
-            public ulong QuotaPeakPagedPoolUsage;
+            public ulong QuotaNonPagedPoolUsage;
 
             /// <summary>
-            /// The current working set size, in bytes (SIZE_T).
+            /// The Commit Charge value in bytes for this process (SIZE_T). Commit Charge is the total amount of memory that the
+            /// memory manager has committed for a running process.
             /// </summary>
-            public ulong WorkingSetSize;
+            public ulong PagefileUsage;
+
+            /// <summary>
+            /// The peak value in bytes of the Commit Charge during the lifetime of this process (SIZE_T).
+            /// </summary>
+            public ulong PeakPagefileUsage;
 
             public PROCESS_MEMORY_COUNTERS()
             {
