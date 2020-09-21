d3481b91063ef3ba328884498dc2c6db2d4b075c
Kein <kein-of@yandex.ru>
2020-09-21 06:38:14 +0300

    Added ConcatFormat (2x slower than manual Concat) (#10)

--- FPSCounter/Extensions/FixedString.cs
+++ FPSCounter/Extensions/FixedString.cs
@@ -3,21 +3,22 @@ using System.Text;
 
 namespace FPSCounter
 {
-    internal class FixedString
+    public class FixedString
     {
         // Constants
         const string MONO20_SB = "_str";
         const string NET35_SB = "m_StringValue";
         const string NET4X_SB = "m_ChunkChars";
         const uint MAX_DECIMALS = 2;
+        const uint DEFAULT_BASE = 10;
         const char DEFAULT_PAD_CHAR = ' ';
         static readonly char[] DIGITS_LUT = new char[]{'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
 
         // Setup fields
         private readonly StringBuilder builder;
-        private string rawValue;
-        private readonly int rawSize;
+        private string rawStrValue;
+        private readonly int definedSize;
         private readonly char[] charData;
         private readonly bool useChunk;
 
@@ -28,7 +29,7 @@ namespace FPSCounter
 
         // Publics
         public int Length { get { return builder.Length; } }
-        public int DefinedSize { get { return rawSize; } }
+        public int DefinedSize { get { return definedSize; } }
         public bool IsChunkImplementation { get { return useChunk; } }
 
         // Own logic
@@ -38,8 +39,8 @@ namespace FPSCounter
             if (strSize <= 0)
                 throw new System.ArgumentException("FixedString length cannot be 0 or less");
 
-            rawSize = strSize;
-            builder = new StringBuilder(rawSize, rawSize);
+            definedSize = strSize;
+            builder = new StringBuilder(definedSize, definedSize);
             var fields = typeof(StringBuilder).GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
             bool found = false;
             for (int i = 0; i < fields.Length; i++)
@@ -47,14 +48,14 @@ namespace FPSCounter
                 useChunk = fields[i].Name.Equals(NET4X_SB);
                 if (useChunk)
                 {
-                    rawValue = new string(' ', rawSize);
+                    rawStrValue = new string(' ', definedSize);
                     charData = (char[])fields[i].GetValue(builder);
                     found = true;
                     break;
                 }
                 else if (fields[i].Name.Equals(MONO20_SB) || fields[i].Name.Equals(NET35_SB))
                 {
-                    rawValue = (string)fields[i].GetValue(builder);
+                    rawStrValue = (string)fields[i].GetValue(builder);
                     found = true;
                     break;
                 }
@@ -71,13 +72,6 @@ namespace FPSCounter
 
             if (useChunk)
             {
-                if (builder.Length >= charData.Length)
-                    throw new ArgumentOutOfRangeException("Builder lenght inconsistent with char array");
-
-                var len = charData.Length;
-                for (int i = builder.Length; i < len; i++)
-                    charData[i] = '\0';
-
                 RawCopyIntoString();
             }
             else
@@ -87,20 +81,24 @@ namespace FPSCounter
             }
 
             builder.Length = 0;
-            return rawValue;
+            return rawStrValue;
         }
 
         private unsafe void RawCopyIntoString()
         {
-            if (rawSize > rawValue.Length || builder.Length > rawSize || charData.Length < rawSize)
-                throw new ArgumentOutOfRangeException("Size mismatch between supplied data and max string size");
+            if (rawStrValue.Length != definedSize || builder.Length > definedSize || charData.Length < definedSize)
+                throw new ArgumentOutOfRangeException(string.Format("Internal structure mismatch! Defined size: {0}, string: {1}, builder: {2}, char buffer: {3}", definedSize, rawStrValue.Length, builder.Length, charData.Length));
 
             unsafe
             {
-                fixed (char* dest_fixed = rawValue)
+                fixed (char* fxdStrPntr = rawStrValue)
                 {
-                    for (int i = 0; i < rawSize; i++)
-                        dest_fixed[i] = charData[i];
+                    var len = builder.Length;
+                    for (int i = 0; i < len; i++)
+                        fxdStrPntr[i] = charData[i];
+
+                    for (int j = len; j < definedSize; j++)
+                        fxdStrPntr[j] = '\0';
                 }
             }
         }
@@ -291,6 +289,11 @@ namespace FPSCounter
         {
             ConcatInternal(ulong_val, pad_amount, pad_char, 10, false);
         }
+
+        public void Concat(ulong ulong_val, uint pad_amount, char pad_char, uint base_val)
+        {
+            ConcatInternal(ulong_val, pad_amount, pad_char, 10, false);
+        }
         #endregion
 
         #region INT64/LONG
@@ -384,5 +387,209 @@ namespace FPSCounter
             Concat(float_val, decimal_places, pad_amount, DEFAULT_PAD_CHAR);
         }
         #endregion
-     }
+
+        #region Format
+        public void ConcatFormat<A>(string format_string, A arg1 )
+            where A : IConvertible
+        {
+            ConcatFormat<A, int, int, int>(format_string, arg1, 0, 0, 0);
+        }
+
+        //! Concatenate a formatted string with arguments
+        public void ConcatFormat<A, B>(string format_string, A arg1, B arg2)
+            where A : IConvertible
+            where B : IConvertible
+        {
+            ConcatFormat<A, B, int, int>(format_string, arg1, arg2, 0, 0);
+        }
+
+        //! Concatenate a formatted string with arguments
+        public void ConcatFormat<A, B, C>(string format_string, A arg1, B arg2, C arg3)
+            where A : IConvertible
+            where B : IConvertible
+            where C : IConvertible
+        {
+            ConcatFormat<A, B, C, int>(format_string, arg1, arg2, arg3, 0);
+        }
+
+        //! Concatenate a formatted string with arguments
+        public void ConcatFormat<A,B,C,D>(string format_string, A arg1, B arg2, C arg3, D arg4)
+            where A : IConvertible
+            where B : IConvertible
+            where C : IConvertible
+            where D : IConvertible
+        {
+            int verbatim_range_start = 0;
+
+            for (int indx = 0; indx < format_string.Length; indx++)
+            {
+                if (format_string[indx] == '{')
+                {
+                    // Formatting bit now, so make sure the last block of the string is written out verbatim.
+                    if (verbatim_range_start < indx)
+                    {
+                        // Write out unformatted string portion
+                        builder.Append(format_string, verbatim_range_start, indx - verbatim_range_start);
+                    }
+
+                    uint base_value = DEFAULT_BASE;
+                    uint padding = 0;
+                    uint decimal_places = MAX_DECIMALS; // Default decimal places in .NET libs
+
+                    indx++;
+                    char format_char = format_string[indx];
+                    if (format_char == '{')
+                    {
+                        builder.Append('{');
+                        indx++;
+                    }
+                    else
+                    {
+                        indx++;
+                        if (format_string[indx] == ':')
+                        {
+                            // Extra formatting. This is a crude first pass proof-of-concept. It's not meant to cover
+                            // comprehensively what the .NET standard library Format() can do.
+                            indx++;
+
+                            // Deal with padding
+                            while (format_string[indx] == '0')
+                            {
+                                indx++;
+                                padding++;
+                            }
+                            if (format_string[indx] == 'X')
+                            {
+                                indx++;
+
+                                // Print in hex
+                                base_value = 16;
+
+                                // Specify amount of padding ( "{0:X8}" for example pads hex to eight characters
+                                if ((format_string[indx] >= '0') && (format_string[indx] <= '9'))
+                                {
+                                    padding = (uint)(format_string[indx] - '0');
+                                    indx++;
+                                }
+                            }
+                            else if (format_string[indx] == '.')
+                            {
+                                indx++;
+
+                                // Specify number of decimal places
+                                decimal_places = 0;
+
+                                while (format_string[indx] == '0')
+                                {
+                                    indx++;
+                                    decimal_places++;
+                                }
+                            }        
+                        }
+                   
+
+                        // Scan through to end bracket
+                        while (format_string[indx] != '}')
+                        {
+                            indx++;
+                        }
+
+                        // Have any extended settings now, so just print out the particular argument they wanted
+                        switch (format_char)
+                        {
+                            case '0': ConcatFormatValue<A>(arg1, padding, base_value, decimal_places); break;
+                            case '1': ConcatFormatValue<B>(arg2, padding, base_value, decimal_places); break;
+                            case '2': ConcatFormatValue<C>(arg3, padding, base_value, decimal_places); break;
+                            case '3': ConcatFormatValue<D>(arg4, padding, base_value, decimal_places); break;
+                            default: Debug.Assert(false, "Invalid parameter index"); break;
+                        }
+                    }
+
+                    // Update the verbatim range, start of a new section now
+                    verbatim_range_start = (indx + 1);
+                }
+            }
+
+            // Anything verbatim to write out?
+            if (verbatim_range_start < format_string.Length)
+            {
+                // Write out unformatted string portion
+                builder.Append(format_string, verbatim_range_start, format_string.Length - verbatim_range_start);
+            }
+        }
+
+        //! The worker method. This does a garbage-free conversion of a generic type, and uses the garbage-free Concat() to add to the stringbuilder
+        private void ConcatFormatValue<T>(T arg, uint padding, uint base_value, uint decimals) where T : IConvertible
+        {
+            switch (arg.GetTypeCode())
+            {
+                case TypeCode.Int64:
+                {
+                    Concat(arg.ToInt64(System.Globalization.NumberFormatInfo.CurrentInfo), padding, DEFAULT_PAD_CHAR, base_value);
+                    break;
+                }
+                case TypeCode.UInt64:
+                {
+                    Concat(arg.ToUInt64(System.Globalization.NumberFormatInfo.CurrentInfo), padding, DEFAULT_PAD_CHAR, base_value);
+                    break;
+                }
+                case TypeCode.UInt32:
+                {
+                    Concat(arg.ToUInt32(System.Globalization.NumberFormatInfo.CurrentInfo), padding, DEFAULT_PAD_CHAR, base_value);
+                    break;
+                }
+                case TypeCode.Int32:
+                {
+                    Concat(arg.ToInt32(System.Globalization.NumberFormatInfo.CurrentInfo), padding, DEFAULT_PAD_CHAR, base_value);
+                    break;
+                }
+                case TypeCode.Single:
+                {
+                    Concat(arg.ToSingle(System.Globalization.NumberFormatInfo.CurrentInfo), decimals, padding, DEFAULT_PAD_CHAR);
+                    break;
+                }
+                case TypeCode.Int16:
+                {
+                    Concat(arg.ToInt32(System.Globalization.NumberFormatInfo.CurrentInfo), padding, DEFAULT_PAD_CHAR, base_value);
+                    break;
+                }
+                case TypeCode.UInt16:
+                {
+                    Concat(arg.ToUInt32(System.Globalization.NumberFormatInfo.CurrentInfo), padding, DEFAULT_PAD_CHAR, base_value);
+                    break;
+                }
+                case TypeCode.Byte:
+                {
+                    Concat(arg.ToUInt32(System.Globalization.NumberFormatInfo.CurrentInfo), padding, DEFAULT_PAD_CHAR, base_value);
+                    break;
+                }
+                case TypeCode.SByte:
+                {
+                    Concat(arg.ToInt32(System.Globalization.NumberFormatInfo.CurrentInfo), padding, DEFAULT_PAD_CHAR, base_value);
+                    break;
+                }
+                case TypeCode.String:
+                {
+                    builder.Append(arg.ToString(System.Globalization.NumberFormatInfo.CurrentInfo));
+                    break;
+                }
+                case TypeCode.Boolean:
+                {
+                    builder.Append(arg.ToBoolean(System.Globalization.NumberFormatInfo.CurrentInfo));
+                    break;
+                }
+                case TypeCode.Char:
+                {
+                    builder.Append(arg.ToChar(System.Globalization.NumberFormatInfo.CurrentInfo));
+                    break;
+                }
+                default:
+                {
+                    throw new ArgumentException("Unknown parameter type");
+                    break;
+                }
+            }
+        }
+        #endregion
+    }
 }
